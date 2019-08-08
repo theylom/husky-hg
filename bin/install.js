0124a38f5502cfb360d356195af451cc1d59119c
Theylom <tebacr@gmail.com>
2019-08-08 11:04:45 -0700

    Initial version

--- bin/install.js
+++ bin/install.js
@@ -1,15 +1,15 @@
-'use strict'
+"use strict";
 
 // Run when package is installed
-const path = require('path')
-const isCI = require('is-ci')
-const installFrom = require('../src/install')
+const path = require("path");
+const isCI = require("is-ci");
+const installFrom = require("../src/install");
 
-console.log('husky')
+console.log("tdog-husky");
 
 if (isCI && !process.env.HUSKY_IGNORE_CI) {
-  console.log('CI detected, skipping Git / hg hooks installation')
-  process.exit(0)
+  console.log("CI detected, skipping Git / hg hooks installation");
+  process.exit(0);
 }
 
 if (process.env.HUSKY_SKIP_INSTALL) {
@@ -17,11 +17,11 @@ if (process.env.HUSKY_SKIP_INSTALL) {
     `env variable HUSKY_SKIP_INSTALL is set to ${
       process.env.HUSKY_SKIP_INSTALL
     }, skipping Git / hg hooks installation`
-  )
-  process.exit(0)
+  );
+  process.exit(0);
 }
 
-console.log('setting up Git / hg hooks')
+console.log("setting up Git / hg hooks");
 
-const huskyDir = path.join(__dirname, '..')
-installFrom(huskyDir)
+const huskyDir = path.join(__dirname, "..");
+installFrom(huskyDir);
--- src/utils/hg-updater.js
+++ src/utils/hg-updater.js
@@ -1,44 +1,44 @@
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const path = require('path')
-const findParent = require('./find-parent')
+const fs = require("fs");
+const path = require("path");
+const findParent = require("./find-parent");
 
-const SECTION_BEGIN = '#husky-hg:begin'
-const SECTION_END = '#husky-hg:end'
+const SECTION_BEGIN = "#tdog-husky-hg:begin";
+const SECTION_END = "#tdog-husky-hg:end";
 
 function containsHook(section, hookName) {
-  return section.data.indexOf(hookName) >= 0
+  return section.data.indexOf(hookName) >= 0;
 }
 
 function findHooksSection(hgrcData) {
-  const hooksSectionStartIndex = hgrcData.indexOf('[hooks]')
-  const lastSection = hgrcData.search(/\[([^\]]*)\][^\[]*$/g)
+  const hooksSectionStartIndex = hgrcData.indexOf("[hooks]");
+  const lastSection = hgrcData.search(/\[([^\]]*)\][^\[]*$/g);
   const hooksSectionEndIndex =
-    lastSection === hooksSectionStartIndex ? hgrcData.length : lastSection - 1
+    lastSection === hooksSectionStartIndex ? hgrcData.length : lastSection - 1;
   let hooksSection = hgrcData.substring(
     hooksSectionStartIndex,
     hooksSectionEndIndex
-  )
+  );
   return {
     start: hooksSectionStartIndex,
     end: hooksSectionEndIndex,
     data: hooksSection
-  }
+  };
 }
 
 function findHuskySection(hgrcData) {
-  const huskySectionStartIndex = hgrcData.indexOf(SECTION_BEGIN)
-  const huskySectionEndIndex = hgrcData.indexOf(SECTION_END)
+  const huskySectionStartIndex = hgrcData.indexOf(SECTION_BEGIN);
+  const huskySectionEndIndex = hgrcData.indexOf(SECTION_END);
   const huskySection = hgrcData.substring(
     huskySectionStartIndex,
     huskySectionEndIndex
-  )
+  );
   return {
     start: huskySectionStartIndex,
     end: huskySectionEndIndex,
     data: huskySection
-  }
+  };
 }
 
 function update(hgrcFile, hgrcData, section) {
@@ -48,84 +48,85 @@ function update(hgrcFile, hgrcData, section) {
       hgrcData.substring(0, section.start) +
         section.data +
         hgrcData.substring(section.end),
-      'utf8'
-    )
+      "utf8"
+    );
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
 
 function hasHooksSection(hgrcData) {
-  return hgrcData.indexOf('[hooks]') > -1
+  return hgrcData.indexOf("[hooks]") > -1;
 }
 function hasHuskySection(hgrcData) {
-  return hgrcData.indexOf(SECTION_BEGIN) > -1
+  return hgrcData.indexOf(SECTION_BEGIN) > -1;
 }
 
-
 function init(huskyDir) {
-  const rootDir = findParent(huskyDir, '.hg')
-  const vcsDir = path.join(rootDir, '.hg')
-  const hgrcFile = path.join(vcsDir, 'hgrc')
+  const rootDir = findParent(huskyDir, ".hg");
+  const vcsDir = path.join(rootDir, ".hg");
+  const hgrcFile = path.join(vcsDir, "hgrc");
   if (!fs.existsSync(hgrcFile)) {
     try {
-      fs.writeFileSync(hgrcFile, `${SECTION_BEGIN}\n[hooks]\n${SECTION_END}`)
+      fs.writeFileSync(hgrcFile, `${SECTION_BEGIN}\n[hooks]\n${SECTION_END}`);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   } else {
     try {
-      const hgrcData = fs.readFileSync(hgrcFile, 'utf8')
+      const hgrcData = fs.readFileSync(hgrcFile, "utf8");
       if (!hasHooksSection(hgrcData)) {
         fs.appendFileSync(
           hgrcFile,
           `\n${SECTION_BEGIN}\n[hooks]\n${SECTION_END}`
-        )
+        );
+      } else if (!hasHuskySection(hgrcData)) {
+        const hooksSection = findHooksSection(hgrcData);
+        hooksSection.data += `\n${SECTION_BEGIN}\n${SECTION_END}`;
+        update(hgrcFile, hgrcData, hooksSection);
       }
-      else if(!hasHuskySection(hgrcData)) {
-        const hooksSection = findHooksSection(hgrcData)
-        hooksSection.data += `\n${SECTION_BEGIN}\n${SECTION_END}`
-        update(hgrcFile, hgrcData, hooksSection)
-      } 
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }
 }
 
 function addHook(huskyDir, hooksdir, hookName) {
-  const rootDir = findParent(huskyDir, '.hg')
-  const vcsDir = path.join(rootDir, '.hg')
-  const hgrcFile = path.join(vcsDir, 'hgrc')
+  const rootDir = findParent(huskyDir, ".hg");
+  const vcsDir = path.join(rootDir, ".hg");
+  const hgrcFile = path.join(vcsDir, "hgrc");
   try {
-    const hgrcData = fs.readFileSync(hgrcFile, 'utf8')
-    const hooksSection = findHooksSection(hgrcData)
-    let huskySection = findHuskySection(hgrcData)
-    if (!containsHook(huskySection, hookName) && !containsHook(hooksSection, hookName)) {
-      huskySection.data += `${hookName}=python:.hg/hooks/${hookName}.py:execute_hook\n`
+    const hgrcData = fs.readFileSync(hgrcFile, "utf8");
+    const hooksSection = findHooksSection(hgrcData);
+    let huskySection = findHuskySection(hgrcData);
+    if (
+      !containsHook(huskySection, hookName) &&
+      !containsHook(hooksSection, hookName)
+    ) {
+      huskySection.data += `pretxncommit.validations = .hg/hooks/validations.py`;
     }
-    update(hgrcFile, hgrcData, huskySection)
+    update(hgrcFile, hgrcData, huskySection);
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
 
 function remove(huskyDir) {
-  const rootDir = findParent(huskyDir, '.hg')
-  const vcsDir = path.join(rootDir, '.hg')
-  const hgrcFile = path.join(vcsDir, 'hgrc')
+  const rootDir = findParent(huskyDir, ".hg");
+  const vcsDir = path.join(rootDir, ".hg");
+  const hgrcFile = path.join(vcsDir, "hgrc");
   try {
-    const hgrcData = fs.readFileSync(hgrcFile, 'utf8')
-    const section = findHuskySection(hgrcData)
+    const hgrcData = fs.readFileSync(hgrcFile, "utf8");
+    const section = findHuskySection(hgrcData);
     fs.writeFileSync(
       hgrcFile,
       hgrcData.substring(0, section.start) +
-        '' +
+        "" +
         hgrcData.substring(section.end + SECTION_END.length),
-      'utf8'
-    )
+      "utf8"
+    );
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
 
@@ -133,4 +134,4 @@ module.exports = {
   init,
   addHook,
   remove
-}
+};
